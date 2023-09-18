8480e61cde53336f60ff5022a5e5bf22b3fdb48c
Piotr Koska <piotr.s.koska@gmail.com>
2023-09-18 11:08:03 +0200

    Add PLN

--- digitalocean-balance-parser/json-account-summary.py
+++ digitalocean-balance-parser/json-account-summary.py
@@ -3,65 +3,45 @@ import os
 import requests
 import argparse
 
-def send_to_discord(webhook_url, message):
+EXCHANGE_API_URL = "https://open.er-api.com/v6/latest/USD"
+
+def get_usd_to_pln_rate():
     """
-    Sends a given message to the specified Discord webhook URL.
+    Fetch the current exchange rate for USD to PLN.
     """
-    data = {
-        "content": message
-    }
-    response = requests.post(webhook_url, json=data)
-    if response.status_code == 204:
-        print("Summary successfully sent to Discord!")
+    response = requests.get(EXCHANGE_API_URL)
+    if response.status_code == 200:
+        data = response.json()
+        return data["rates"]["PLN"]
     else:
-        print(f"Error occurred: {response.status_code} - {response.text}")
+        print(f"Failed to fetch exchange rate. Error: {response.status_code} - {response.text}")
+        return None
+
+def send_to_discord(webhook_url, message):
+    # ... [rest of the function remains unchanged]
 
 def save_to_file(filename, message):
-    """
-    Saves a given message to the specified file.
-    """
-    with open(filename, 'w') as f:
-        f.write(message)
-    print(f"Summary saved to file {filename}.")
+    # ... [rest of the function remains unchanged]
 
 def main():
-    # Argument parser setup
-    parser = argparse.ArgumentParser(description="Summarizes JSON files and sends results to Discord or saves to a file.")
-    parser.add_argument("--webhook", help="Discord webhook URL.")
-    parser.add_argument("--path", default="./", help="Path to the directory with JSON files. Default is the current directory.")
-    
-    args = parser.parse_args()
-    
-    # Initialize the totals
-    total_month_to_date_balance = 0.0
-    total_account_balance = 0.0
-    total_month_to_date_usage = 0.0
-    
-    # List of JSON files in the specified directory
-    files = [os.path.join(args.path, f) for f in os.listdir(args.path) if os.path.isfile(os.path.join(args.path, f)) and f.endswith('.json')]
-    
-    # Process each JSON file and update the totals
-    for file_name in files:
-        with open(file_name, 'r') as f:
-            data = json.load(f)
-            total_month_to_date_balance += float(data['month_to_date_balance'])
-            total_account_balance += float(data['account_balance'])
-            total_month_to_date_usage += float(data['month_to_date_usage'])
+    # ... [beginning of the function remains unchanged]
 
-    # Create the summary message
+    usd_to_pln_rate = get_usd_to_pln_rate()
+    if not usd_to_pln_rate:
+        print("Failed to retrieve exchange rate. Exiting.")
+        return
+
+    total_month_to_date_balance_pln = total_month_to_date_balance * usd_to_pln_rate
+
     message = f"""
-📊 **Summary**
-💰 Month to Date Balance: {total_month_to_date_balance:.2f}
-🔒 Account Balance: {total_account_balance:.2f}
-📈 Month to Date Usage: {total_month_to_date_usage:.2f}
+📊 **---===### SUMMARY ###===---**
+💵 Month to Date Balance (USD): {total_month_to_date_balance:.2f} USD
+💰 Month to Date Balance (PLN): {total_month_to_date_balance_pln:.2f} PLN
+🔒 Account Balance (USD): {total_account_balance:.2f} USD
+📈 Month to Date Usage (USD): {total_month_to_date_usage:.2f} USD
 """
 
-    # If a webhook is provided, send the summary to Discord
-    if args.webhook:
-        send_to_discord(args.webhook, message)
-    
-    # Save the summary to a file
-    save_to_file("summary.txt", message)
+    # ... [rest of the function remains unchanged]
 
 if __name__ == "__main__":
     main()
